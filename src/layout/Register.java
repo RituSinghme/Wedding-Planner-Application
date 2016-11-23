4c04a6e4607387bd4a0e615814df43151734f93e
anu3990 <anushree0390@gmail.com>
2016-11-23 13:14:50 -0800

    Update Register.java

--- src/layout/Register.java
+++ src/layout/Register.java
@@ -1,4 +1,4 @@
-package layout;
+package takeone;
 
 import java.sql.DriverManager;
 
@@ -36,66 +36,86 @@ public class Register extends HttpServlet{
 		 Connection connection = null;
 	     PreparedStatement pstatement = null;
 	     
-	     DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy",Locale.ENGLISH);
-	        String dateInString = w_date;
-	        Date date = null;
-	        java.sql.Date sqlDate = null;
-
-	        try {
-	        	if(dateInString!=null)
-	        	{
-	        		date = formatter.parse(dateInString);
-	        		sqlDate = new java.sql.Date(date.getTime());
-	        	}
-	            
-
-	        } catch (ParseException e) {
-	            e.printStackTrace();
-	        }
-	    
+	     	        
 	        
-	        
-	          int updateQuery = 0;
-	          int updateQuery2 = 0;
+	          int insertQuery1 = 0;
+	          int insertQuery2 = 0;
+	          int insertQuery3 =0;
+	          int insertQuery4 = 0;
 		                 try {
 		                	 Class.forName("com.mysql.jdbc.Driver");
 		            	     
-		            	     connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Wedding_Planner?useSSL=false","root","passwordbaru");
-	              String queryString = ("insert into customer(email, groom_name, bride_name, contact_person, w_date, booking_date) VALUES (?, ?, ?, ?, ?, CURDATE())");
+		            	     connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Wedding_Planner?useSSL=false","root","anu123");
+	              String queryString = ("insert into customer(email, groom_name, bride_name, contact_person, w_date, booking_date) VALUES (?, ?, ?, ?,cast(w_date as DATETIME), CURDATE())");
 	              	     
 	              pstatement = connection.prepareStatement(queryString);
 	              pstatement.setString(1, email);
 				  pstatement.setString(2, groom_name);
 				  pstatement.setString(3, bride_name);
 	              pstatement.setString(4, contact_person);
-	              pstatement.setDate(5, sqlDate);
+	             // pstatement.setString(5, cast(w_date as DATETIME));
 	             
 	              
-	              updateQuery = pstatement.executeUpdate();
+	              insertQuery1 = pstatement.executeUpdate();
 	              pstatement = null;
 	              
 	              String Query = ("select max(c_id) from customer");
 	              pstatement = connection.prepareStatement(Query);
 	              ResultSet rs = pstatement.executeQuery();
-	              if(rs.next())
-	              {
-	              String customer_id = rs.getString(1);
-	              
-	              String queryString2 = ("insert into member(user_name,password,c_id) values (?,?,?)");
-	              pstatement = connection.prepareStatement(queryString2);
-	              pstatement.setString(1, user_name);
-	              pstatement.setString(2, password);
-	              pstatement.setString(3, customer_id);
-	              updateQuery2 = pstatement.executeUpdate();
-	              session.setAttribute("customer_id", customer_id);
-	              session.setAttribute("wedding_date", sqlDate);
-	              }
-	              
-	              if(updateQuery!=0 && updateQuery2!=0)
-	            	  success=true;
-	                            
-	            } 
-	            catch (Exception ex) {
+
+			if (rs != null && rs.next()) {
+				String customer_id = rs.getString(1);
+
+				String queryString2 = ("insert into member(user_name,password,c_id) values (?,?,?)");
+				pstatement = connection.prepareStatement(queryString2);
+				pstatement.setString(1, user_name);
+				pstatement.setString(2, password);
+				pstatement.setString(3, customer_id);
+				insertQuery2 = pstatement.executeUpdate();
+
+				String query3 = ("insert into wedding(c_id,w_date) values (?,cast(w_date as DATETIME))");
+				pstatement = connection.prepareStatement(query3);
+				pstatement.setString(1, customer_id);
+				//pstatement.setString(2, sqlDate.toString());
+				insertQuery3 = pstatement.executeUpdate();
+
+				if (insertQuery3 != 0 ) {
+					Query = ("select max(w_id) from wedding");
+					pstatement = connection.prepareStatement(Query);
+					ResultSet rs3 = pstatement.executeQuery();
+					if (rs3.next())
+					{
+						int wedding_id = rs3.getInt(1);
+						session.setAttribute("wedding_id", wedding_id);
+					
+					String query4 = ("insert into wedding_component(w_id) values ("+ wedding_id + ")");
+					pstatement = connection.prepareStatement(query4);
+					insertQuery4 = pstatement.executeUpdate();
+					
+					String Query5 = ("select max(wc_id) from wedding_component");
+					pstatement = connection.prepareStatement(Query5);
+					ResultSet rs4 = pstatement.executeQuery();
+					if(rs4.next())
+						session.setAttribute("wc_id", rs4.getString(1));
+					
+					//String query5 = ("select max(wc_id) from wedding")
+					}
+					
+				}
+
+				session.setAttribute("customer_id", customer_id);
+				session.setAttribute("wedding_date", w_date);
+				String cus = session.getAttribute("customer_id").toString();
+				String wed_date = session.getAttribute("wedding_date").toString();
+				String wed_id = session.getAttribute("wedding_id").toString();
+				String wed_compo = session.getAttribute("wc_id").toString();
+				
+			}
+
+			if (insertQuery1 != 0 && insertQuery2 != 0 && insertQuery3!=0 && insertQuery4!=0)
+				success = true;
+
+		}         catch (Exception ex) {
 	            System.out.println("Unable to connect to database.");
 	   
 	               }
